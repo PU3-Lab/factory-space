d3d0a1c163071360470f1c8a5776c4172735b1ad
ssr5087 <ssr5087@gmail.com>
2026-06-02 18:41:28 +0900

    그라인더 파일 삭제 후 생성
    제곧내

---
+++ frontend/Source/Wanted_Factory/Machines/Grinder.cpp
@@ -0,0 +1,156 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Grinder.h"
+
+#include "RecipeManagerSubsystem.h"
+#include "Wanted_Factory.h"
+
+// Sets default values
+AGrinder::AGrinder()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	MachineType = TEXT("Grinder");
+	InputPortCount = 1;
+	OutputPortCount = 1;
+
+	InputPorts.Reset();
+	FMachinePortData InputPort;
+	InputPort.PortIndex = 0;
+	InputPort.PortType = EPortType::Input;
+	InputPorts.Add(InputPort);
+
+	OutputPorts.Reset();
+	FMachinePortData OutputPort;
+	OutputPort.PortIndex = 0;
+	OutputPort.PortType = EPortType::Output;
+	OutputPorts.Add(OutputPort);
+}
+
+// Called when the game starts or when spawned
+void AGrinder::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGrinder::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+bool AGrinder::AddItem(FName ItemID, int32 Count)
+{
+	if (!CanReceiveConveyorItem(ItemID, Count))
+	{
+		LOG_SSR_W(TEXT("Grinder rejected input item: %s"), *ItemID.ToString());
+		return false;
+	}
+
+	return Super::AddItem(ItemID, Count);
+}
+
+void AGrinder::AddOutputItem(FName ItemID, int32 Count)
+{
+	for (const TPair<FName, int32>& Output : OutputBuffer)
+	{
+		if (!Output.Key.IsNone() && Output.Value > 0 && Output.Key != ItemID)
+		{
+			MachineState = EMachineState::Blocked;
+
+			LOG_SSR_W(
+				TEXT("Grinder output buffer already contains another item: %s"),
+				*Output.Key.ToString()
+			);
+			return;
+		}
+	}
+
+	Super::AddOutputItem(ItemID, Count);
+}
+
+bool AGrinder::CanAddToOutputBuffer(const FRecipeTable& Recipe) const
+{
+	int32 OutputItemCount = 0;
+	FName RecipeOutputItem = NAME_None;
+
+	auto CollectOutput = [&OutputItemCount, &RecipeOutputItem](FName ItemID, int32 Qty)
+	{
+		if (ItemID.IsNone() || Qty <= 0)
+		{
+			return;
+		}
+
+		++OutputItemCount;
+		RecipeOutputItem = ItemID;
+	};
+
+	CollectOutput(Recipe.OutputItem1, Recipe.OutputQty1);
+	CollectOutput(Recipe.OutputItem2, Recipe.OutputQty2);
+
+	if (OutputItemCount > 1)
+	{
+		return false;
+	}
+
+	for (const TPair<FName, int32>& Output : OutputBuffer)
+	{
+		if (!Output.Key.IsNone() && Output.Value > 0 && Output.Key != RecipeOutputItem)
+		{
+			return false;
+		}
+	}
+
+	return Super::CanAddToOutputBuffer(Recipe);
+}
+
+bool AGrinder::CanReceiveConveyorItem(FName ItemID, int32 Count) const
+{
+	if (!Super::CanReceiveConveyorItem(ItemID, Count))
+	{
+		return false;
+	}
+
+	URecipeManagerSubsystem* RecipeManager =
+		GetGameInstance() ? GetGameInstance()->GetSubsystem<URecipeManagerSubsystem>() : nullptr;
+
+	if (!RecipeManager)
+	{
+		return false;
+	}
+
+	TArray<FRecipeTable> FoundRecipes;
+	if (!RecipeManager->FindRecipesByInputItem(ItemID, FoundRecipes))
+	{
+		return false;
+	}
+
+	bool bHasGrinderRecipe = false;
+	for (const FRecipeTable& Recipe : FoundRecipes)
+	{
+		if (Recipe.MachineType == MachineType)
+		{
+			bHasGrinderRecipe = true;
+			break;
+		}
+	}
+
+	if (!bHasGrinderRecipe)
+	{
+		return false;
+	}
+
+	for (const TPair<FName, int32>& Input : InputInventory)
+	{
+		if (!Input.Key.IsNone() && Input.Value > 0 && Input.Key != ItemID)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
