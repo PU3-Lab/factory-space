937d3884ec6396b57ef98285978b67fa4c56aa68
Chan <chanlee420@naver.com>
2026-06-27 11:48:52 +0900

    플레이어 야간 조명 수정, 튜토리얼 대사 수정, 데이터 테이블 csv 수정

--- frontend/Source/Wanted_Factory/Private/OJJ_Player.cpp
+++ frontend/Source/Wanted_Factory/Private/OJJ_Player.cpp
@@ -87,7 +87,7 @@ AOJJ_Player::AOJJ_Player()
 
 	NightSpotLight = CreateDefaultSubobject<USpotLightComponent>(TEXT("NightSpotLight"));
 	NightSpotLight->SetupAttachment(RootComponent);
-	NightSpotLight->SetRelativeLocation(FVector(10.0f, 0.0f, 25.0f));
+	NightSpotLight->SetRelativeLocation(FVector(25.0f, 0.0f, 30.0f));
 	NightSpotLight->SetRelativeRotation(FRotator(0.0f, 0.0f, 0.0f));
 	NightSpotLight->Intensity = 50.f;
 	NightSpotLight->AttenuationRadius = 1500.f;
@@ -1655,7 +1655,7 @@ void AOJJ_Player::BuildRotate(const FInputActionValue& Value)
 		return;
 	}
 
-	const float RotateInput = Value.Get<float>() * -1;
+	const float RotateInput = Value.Get<float>();
 
 	if (BuildCamera)
 	{
