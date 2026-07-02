15e74143e51a350daf2aea044efa98815f5251e2
Chan <chanlee420@naver.com>
2026-07-02 21:41:19 +0900

    발전소 내구도 감소, 최적화 에이전트 통신

--- frontend/Source/Wanted_Factory/Private/FactoryAgentJsonUtils.h
+++ frontend/Source/Wanted_Factory/Private/FactoryAgentJsonUtils.h
@@ -8,6 +8,7 @@ namespace FactoryAgentJsonUtils
 {
 	bool ParseJsonObject(const FString& Json, TSharedPtr<FJsonObject>& OutObject);
 	FString WriteJsonObject(const TSharedPtr<FJsonObject>& JsonObject);
+	FString WritePrettyJsonObject(const TSharedPtr<FJsonObject>& JsonObject);
 	FString GetStringField(const TSharedPtr<FJsonObject>& JsonObject, const TCHAR* FieldName);
 	int32 GetIntegerField(const TSharedPtr<FJsonObject>& JsonObject, const TCHAR* FieldName, int32 DefaultValue);
 	TSharedPtr<FJsonObject> GetObjectField(const TSharedPtr<FJsonObject>& JsonObject, const TCHAR* FieldName);
